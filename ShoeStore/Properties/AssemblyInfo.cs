d1309a6216dd7e37b0c054a4ae3307ab1eaca581
Eva Evans <kuchiyosedoton@gmail.com>
2026-04-15 13:29:32 +0500

    Обновление от сегодня2

--- ShoeStore/Properties/AssemblyInfo.cs
+++ ShoeStore/Properties/AssemblyInfo.cs
@@ -7,6 +7,7 @@ using System.Windows;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набор атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
+
 [assembly: AssemblyTitle("ShoeStore")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
